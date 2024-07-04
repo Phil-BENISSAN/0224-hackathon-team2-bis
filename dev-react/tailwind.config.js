23517ae51f9ee60742cf211b2fd80881e1e38777
ALEXANDRE MORO <alexandre.moro@live.fr>
2024-07-04 20:26:05 +0200

    indent tailwind config

--- dev-react/tailwind.config.js
+++ dev-react/tailwind.config.js
@@ -1,25 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   daisyui: {
     themes: [
       {
         mytheme: {
-          
-"primary": "#ffb782",
-"secondary": "#0081a8",
-"accent": "#43135b",
-"neutral": "#2f2f2f",
-"base-100": "#ffffff",
-          },
+          primary: "#ffb782",
+          secondary: "#0081a8",
+          accent: "#43135b",
+          neutral: "#2f2f2f",
+          "base-100": "#ffffff",
         },
-      ],
-    },
-  plugins: [
-    require('daisyui'),
-  ],
-}
-
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
