e1da746d522e0f5ce267a826bf8b60cac882beba
FleauLePreux <quentin.tranhuu@hotmail.com>
2024-07-04 21:30:30 +0200

    API c'est le dernier !

--- data-fastapi/app/ML.py
+++ data-fastapi/app/ML.py
@@ -6,14 +6,20 @@ from sklearn.neighbors import NearestNeighbors
 
 df_recruteurs = pd.read_csv('app/data_recruters_needs.csv')
 df_clients = pd.read_csv('app/data_candidates_scores.csv')
+df_name = pd.read_csv('app/infos_candidates.csv')
+
 
 ############# Machine Learning Training #############
 
 
 def get_candidate_id(employeur_id):
+    
     col_of_interest_recruteur = [col for col in df_recruteurs.columns if df_recruteurs[col].values[employeur_id] == 1]
+    
     X = df_clients[col_of_interest_recruteur]
+    
     my_model = NearestNeighbors(n_neighbors=4).fit(X)
-    df = pd.DataFrame(data = my_model.kneighbors(df_recruteurs[col_of_interest_recruteur])[1][0])
-
-    return df.T.to_dict(orient='index')
+    
+    idcloser_candidate = my_model.kneighbors(df_recruteurs[col_of_interest_recruteur])[1][0]
+        
+    return df_name.iloc[idcloser_candidate, :].to_dict(orient='index')
