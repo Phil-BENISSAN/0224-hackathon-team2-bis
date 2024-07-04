3cf8720d8cc159c9daf259f8dc95a58e05e083d3
ALEXANDRE MORO <alexandre.moro@live.fr>
2024-07-04 22:00:09 +0200

    change style them.jsx

--- dev-react/src/pages/Theme.jsx
+++ dev-react/src/pages/Theme.jsx
@@ -13,8 +13,8 @@ export default function Theme() {
         <div className="card bg-base-100 image-full w-96 shadow-xl">
           <figure>
             <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
+              src=""
+              alt=""
             />
           </figure>
           <div className="card-body">
@@ -25,7 +25,7 @@ export default function Theme() {
                 onClick={() => handleClick("/question")}
                 className="btn btn-primary"
               >
-                Buy Now
+                Commencer
               </button>
             </div>
           </div>
@@ -33,8 +33,8 @@ export default function Theme() {
         <div className="card bg-base-100 image-full w-96 shadow-xl">
           <figure>
             <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
+              src=""
+              alt=""
             />
           </figure>
           <div className="card-body">
@@ -45,7 +45,7 @@ export default function Theme() {
                 onClick={() => handleClick("/select_level_sql")}
                 className="btn btn-primary"
               >
-                Buy Now
+                Commencer
               </button>
             </div>
           </div>
@@ -53,8 +53,8 @@ export default function Theme() {
         <div className="card bg-base-100 image-full w-96 shadow-xl">
           <figure>
             <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
+              src=""
+              alt=""
             />
           </figure>
           <div className="card-body">
@@ -65,7 +65,7 @@ export default function Theme() {
                 onClick={() => handleClick("/select_level_python")}
                 className="btn btn-primary"
               >
-                Buy Now
+                Commencer
               </button>
             </div>
           </div>
@@ -73,8 +73,8 @@ export default function Theme() {
         <div className="card bg-base-100 image-full w-96 shadow-xl">
           <figure>
             <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
+              src=""
+              alt=""
             />
           </figure>
           <div className="card-body">
@@ -85,16 +85,22 @@ export default function Theme() {
                 onClick={() => handleClick("/select_level_powerBI")}
                 className="btn btn-primary"
               >
-                Buy Now
+                Commencer
               </button>
             </div>
           </div>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center gap bg-[#f8eeec] h-[800px]">
-        <h2 className="text-6xl mb-24 text-accent">
+        <h2 className="text-6xl text-accent mb-4">
           Quelques chiffres de notre site
         </h2>
+        <p className="mb-24 w-[700px]">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas
+          dolorem quibusdam debitis! Corporis hic eius illo blanditiis sint
+          architecto, temporibus dolores quasi possimus distinctio tenetur
+          deserunt aspernatur sequi voluptatibus quaerat?
+        </p>
         <Rating />
       </div>
     </main>
